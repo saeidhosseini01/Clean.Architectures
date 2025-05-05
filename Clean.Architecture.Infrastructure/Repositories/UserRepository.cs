2bac293fa7b343437a16314cc501910b334f7d68
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-05-06 01:02:55 +0430

    UserRepository

---
+++ Clean.Architecture.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,49 @@
+﻿using Clean.Architecture.Domain.Entities;
+using Clean.Architecture.Domain.Exeptions;
+using Clean.Architecture.Domain.Interfaces;
+using Clean.Architecture.Persistence.ApiDbContext;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean.Architecture.Infrastructure.Repositories
+{
+
+
+
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApiDbContext _context;
+
+        public UserRepository(ApiDbContext context)
+        {
+          _context = context;
+        }
+        public async Task AddUserAsync(User user, CancellationToken cancellationToken)
+        {
+          await _context.AddAsync(user,cancellationToken);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task<List<User>> GetAllUserAsync(CancellationToken cancellationToken)
+        => _context.User.ToListAsync(cancellationToken);
+
+        public async Task<User> GetUserByIdAsync(string id, CancellationToken cancellationToken)
+        => _context.User.FindAsync(id, cancellationToken);
+
+        public async Task UpdateUserAsync(User user, CancellationToken cancellationToken)
+        {
+            var found = await _context.User.FindAsync([user.Id], cancellationToken);
+            if (found == null) throw new NotFountUserExeption();
+
+            found.Age = user.Age;
+            found.Name = user.Name;
+            found.family=user.family;
+            await _context.SaveChangesAsync(cancellationToken);
+
+        }
+    }
+}
---
+++ Persistance/ApiDbContext/ApiDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+using System.Threading.Tasks;
+using Clean.Architecture.Domain.Entities;
+
+
+
+
+
+namespace Clean.Architecture.Persistence.ApiDbContext
+{
+
+
+    public class ApiDbContext(DbContextOptions<ApiDbContext> options):DbContext(options)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(user =>
+            {
+                user.Property(e => e.Id);
+                user.HasKey(e => e.Id);
+            });
+        }
+        public DbSet<User> User { get; set; }
+    }
+}
