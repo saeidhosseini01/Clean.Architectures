18f886df0ffc6a77fae2ebea0cff55ca7e3648a1
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-05-07 01:45:09 +0430

    AutoMapperConfigs

---
+++ Clean.Architecture.Application/Mappings/Mappings.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Clean.Architecture.Application.Dtos;
+using Clean.Architecture.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean.Architecture.Application.Mappings
+{
+    public class MappingsProfile:Profile
+    {
+        public MappingsProfile()
+        {
+                
+        CreateMap<User, UserDto>().ReverseMap();
+        }
+    }
+}
--- Clean.Architecture.Presentations/Program.cs
+++ Clean.Architecture.Presentations/Program.cs
@@ -1,4 +1,9 @@
+using Clean.Architecture.Application.Mappings;
+using Clean.Architecture.Domain.Interfaces;
+using Clean.Architecture.Infrastructure.Repositories;
+using Clean.Architecture.Persistence.ApiDbContext;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,14 +13,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 
-builder.Services.AddAutoMapper();
+builder.Services.AddAutoMapper(typeof(MappingsProfile));
 
 builder.Services.AddMediatR(cfg =>
 {
     cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
 });
 
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
+builder.Services.AddDbContext<ApiDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Server=.;Database=MyCacheDb;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;")));
 
 
 
