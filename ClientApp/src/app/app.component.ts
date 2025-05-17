ee339af0303ef7f624288ef2b0ea3181964fce5c
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-05-18 01:14:23 +0430

    اولین اتصال انگولار به دات نت

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { json, text } from 'stream/consumers';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
+  users: any[] = [];
+  constructor (private http:HttpClient){};
+  ngOnInit(): void {
+    
+    this.http.get<any[]>('https://localhost:7139/api/user/getAllUsers').subscribe({
+      
+      next:data=> this.users=data,
+      error :err => console.error('api error')
+    })
+  }
+
+
   title = 'ClientApp';
 }
