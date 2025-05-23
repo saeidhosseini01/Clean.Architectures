2ee340552f3a37ba1ec01591db3a1aa9f5e14aba
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-05-24 00:07:20 +0430

    syncAdminLayout

--- Clean.Architecture.EndPoint/lawyer-website/src/app/layout/admin-layout/admin-layout.component.ts
+++ Clean.Architecture.EndPoint/lawyer-website/src/app/layout/admin-layout/admin-layout.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [],
   templateUrl: './admin-layout.component.html',
-  styleUrl: './admin-layout.component.css'
+  styleUrls: ['./admin-layout.component.css']
 })
-export class AdminLayoutComponent {
-
-}
+export class AdminLayoutComponent { }
