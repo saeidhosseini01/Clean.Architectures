4f18dbda5a564ec4c89d83e9e38fa71353462fe1
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-05-29 02:59:46 +0430

    cnstTypeValueObj

---
+++ Clean.Architecture/ValueObject/ConstType/TypeId.cs
@@ -0,0 +1,8 @@
+﻿namespace Clean.Architecture.Domain.ValueObject.ConstType
+{
+    public record TypeId(int value)
+    {
+        public static implicit operator int(TypeId typeId) => typeId.value;
+    }
+
+}
---
+++ Clean.Architecture/ValueObject/ConstType/TypeTitle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean.Architecture.Domain.ValueObject.ConstType
+{
+    public record TypeTitle(string value)
+    {
+        public static implicit operator string(TypeTitle title) => title.value; 
+    }
+
+}
