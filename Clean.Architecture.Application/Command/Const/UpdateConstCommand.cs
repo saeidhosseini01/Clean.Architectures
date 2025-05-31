c3265ec8eace2dfd4eaf08b0589146f67d8cdbab
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-06-01 02:33:39 +0430

    UpdateConstCommand

--- Clean.Architecture.Application/Command/Const/UpdateConstCommand.cs
+++ Clean.Architecture.Application/Command/Const/UpdateConstCommand.cs
@@ -1,14 +1,28 @@
 ﻿using Clean.Architecture.Application.Dtos.Common;
 using Clean.Architecture.Domain.ValueObject.Const;
+using Clean.Architecture.Domain.ValueObject.Genaric;
+
 using MediatR;
 
 namespace Clean.Architecture.Application.Command.Const
 {
-    public class UpdateConstCommand(Name name, Key key, Order order, Value value) : IRequest<ConstDto>
+    public class UpdateConstCommand : IRequest<ConstDto>
     {
-        public Name Name { get; set; } = name;
-        public Key Key { get; set; } = key;
-        public Order Order { get; set; } = order;
-        public Value Value { get; set; } = value;
+
+        public Name    Name { get; set; }  
+        public Key     Key { get; set; }   
+        public Order  Order { get; set; } 
+        public Value  Value { get; set; } 
+        public Description  Description { get; set; }
+        public Guid Id { get; set; } 
+
+        public UpdateConstCommand(Guid Id, Name name, Key key, Order order, Value value , Description description)
+        {
+            Name = name;
+            Key = key;
+            Order = order;
+            Value = value;
+            Description = description;
+        }
     }
 }
---
+++ Clean.Architecture.Application/Handlers/CommandHandler/Consts/UpdateConstCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Clean.Architecture.Application.Command.Const;
+using Clean.Architecture.Application.Dtos.Common;
+using Clean.Architecture.Domain.Entities.Common;
+using Clean.Architecture.Domain.Interfaces.Consts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean.Architecture.Application.Handlers.CommandHandler.Consts
+{
+    public class UpdateConstCommandHandler:IRequestHandler<UpdateConstCommand,ConstDto>
+    {
+        private readonly IConstRepository _constRepository;
+        private readonly IMapper _mapper;
+
+        public UpdateConstCommandHandler(IConstRepository constRepository,IMapper mapper)
+        {
+        _constRepository = constRepository;
+         _mapper = mapper;
+        }
+
+        public async Task<ConstDto> Handle(UpdateConstCommand request, CancellationToken cancellationToken)
+        {
+            Const res =await  _constRepository.GetConstByIdAsync(request.Id, cancellationToken);
+            if(res is null) { }
+            res.Order=request.Order;
+            res.Name=request.Name;
+            res.Description=request.Description;    
+            res.Key=request.Key;
+            await _constRepository.UpdateConstAsync(res, cancellationToken);
+            return _mapper.Map<ConstDto>( _constRepository.GetConstByIdAsync(request.Id, cancellationToken));
+
+        }
+    }
+}
