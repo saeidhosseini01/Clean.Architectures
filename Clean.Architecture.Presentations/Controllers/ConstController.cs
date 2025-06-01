72f26b02c201afffeac7a6a20610979b9943e9a8
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-06-02 02:21:54 +0430

    createConstController

---
+++ Clean.Architecture.Presentations/Controllers/ConstController.cs
@@ -0,0 +1,19 @@
+﻿using Clean.Architecture.Domain.Entities.Common;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clean.Architecture.WebApi.Controllers
+{
+    public class ConstController : Controller
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<Const> _logger;
+
+        public ConstController(IMediator mediator,ILogger<Const> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+    }
+}
