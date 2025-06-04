b31a3d7729465c7cf761b1e01954597c6dec589e
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-06-04 23:57:08 +0430

    CreateTValueDto

---
+++ Clean.Architecture.Application/Dtos/Base/TValue.cs
@@ -0,0 +1,38 @@
+﻿using Clean.Architecture.Domain.Interfaces.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean.Architecture.Application.Dtos.Base
+{
+    public class TValue<T> : IValueWrapper<T>
+    {
+        public T? Value { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public int? IntData { get; set; }
+        public string? StringData { get; set; }
+
+        public TValue() { }
+
+        public TValue(T? value, string? title, string? description = null,
+            int? intData = null, string? stringData = null)
+        {
+            Value = value;
+            Title = title;
+            Description = description;
+            IntData = intData;
+            StringData = stringData;
+        }
+
+        public TValue(T value, string title)
+        {
+            Value = value;
+            Title = title;
+        }
+    }
+
+}
---
+++ Clean.Architecture/Interfaces/Common/IValueWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean.Architecture.Domain.Interfaces.Common
+{
+    public interface IValueWrapper<T>
+    {
+        T? Value { get; set; }
+        string? Title { get; set; }
+    }
+}
