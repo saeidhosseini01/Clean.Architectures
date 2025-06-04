ce2c6cb213f962d36607c124753f004d5fc800d6
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-06-05 00:37:05 +0430

    migration

---
+++ Persistance/Migrations/20250604200628_setkeyConsttype.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Clean.Architecture.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class setkeyConsttype : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConstTypeIds",
+                schema: "CNT",
+                table: "Const");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Key",
+                schema: "CNT",
+                table: "ConstType",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ConstTypeId",
+                schema: "CNT",
+                table: "Const",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Key",
+                schema: "CNT",
+                table: "ConstType");
+
+            migrationBuilder.DropColumn(
+                name: "ConstTypeId",
+                schema: "CNT",
+                table: "Const");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConstTypeIds",
+                schema: "CNT",
+                table: "Const",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
