a9bd8a6d99a485a1e7fd17f4e75bb4825bf3328e
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-06-07 01:37:53 +0430

    loginCommandModel

---
+++ Clean.Architecture.Application/Command/Login/LoginCommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean.Architecture.Application.Command.Login
+{
+    public class LoginCommand : IRequest<string>
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public LoginCommand(string username, string password)
+        {
+            
+            this.Username = username;
+            this.Password = password;
+        }
+    }
+}
