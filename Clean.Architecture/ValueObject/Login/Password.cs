e66d7b714c5b0948c2229cb3828b5ebce571fe8b
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-06-07 01:47:52 +0430

    LoginObjectValue

---
+++ Clean.Architecture/ValueObject/Login/Password.cs
@@ -0,0 +1,7 @@
+﻿namespace Clean.Architecture.Domain.ValueObject.Login
+{
+    public record Password(string value)
+    {
+        public static implicit operator string(Password password)=> password.value;
+    }
+}
---
+++ Clean.Architecture/ValueObject/Login/UserName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean.Architecture.Domain.ValueObject.Login
+{
+    public record UserName(string value)
+    {
+        public static implicit operator string(UserName username) => username.value;
+    }
+}
