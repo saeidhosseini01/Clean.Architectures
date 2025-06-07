82b60fd30efc213c86a4b743555d25e7cac3949d
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-06-08 00:04:48 +0430

    AddUserValidation

---
+++ Clean.Architecture.Application/FluentValidator/User/CreateUserValidator .cs
@@ -0,0 +1,26 @@
+﻿using Clean.Architecture.Application.Command.User;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean.Architecture.Application.FluentValidator.User
+{
+    public class CreateUserValidator : AbstractValidator<AddUserCommand>
+    {
+    {
+        public CreateUserValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage(" نام نمیتواند خالی باشد")
+             .MinimumLength(3).WithMessage("حداقل ۳ کاراکتر");
+
+            RuleFor(x => x.Family)
+                .NotEmpty().WithMessage("ایمیل الزامی است")
+                .EmailAddress().WithMessage("ایمیل نامعتبر است");
+
+        }
+    }
+}
