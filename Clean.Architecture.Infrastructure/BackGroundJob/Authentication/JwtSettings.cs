7d84ad8e168cb5243c48f55a335328c2f6fa458d
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-06-11 02:14:24 +0430

    sync

--- Clean.Architecture.Infrastructure/BackGroundJob/Authentication/JwtSettings.cs
+++ Clean.Architecture.Infrastructure/BackGroundJob/Authentication/JwtSettings.cs
@@ -11,6 +11,7 @@ namespace Clean.Architecture.Infrastructure.BackGroundJob.Authentication
         public string Key { get; set; } = string.Empty;
         public string Issuer { get; set; } = string.Empty;
         public string Audience { get; set; } = string.Empty;
-        public int DurationInMinutes { get; set; }
+        public int AccessTokenExpirationMinutes { get; set; }
+        public int RefreshTokenExpirationDays { get; set; }
     }
 }
---
+++ Clean.Architecture/Entities/Jwt/RefreshToken.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean.Architecture.Domain.Entities.Jwt
+{
+    public class RefreshToken
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Token { get; set; }
+        public DateTime Expires { get; set; }
+        public bool IsExpired => DateTime.UtcNow >= Expires;
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime? Revoked { get; set; }
+        public bool IsActive => Revoked == null && !IsExpired;
+
+        public Guid UserId { get; set; } // یا int بسته به پروژه
+        //public ApplicationUser User { get; set; } // نگاشت EF Core
+    }
+
+}
